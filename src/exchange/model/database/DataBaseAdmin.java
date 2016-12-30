d427e136e1fc0b3d1732412bc312376832fbf601
LuoYouRen <s9016010@gmail.com>
2016-12-30 22:39:40 +0800

    DataBaseAdmin Ver1.1 (add comments)

--- src/exchange/model/database/DataBaseAdmin.java
+++ src/exchange/model/database/DataBaseAdmin.java
@@ -8,27 +8,29 @@ import java.sql.SQLException;
 
 public  class DataBaseAdmin {
 	
-	private static  Connection connection = null;
+	private static  Connection connection = null; //用以跟資料庫連接之物件
 
 
 	//建立連線
+	//參數為資料庫帳號密碼
 	public static void openConnection(String user,String pwd) {
 		System.out.println("-------- MySQL JDBC Connection ------------");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			System.out.println("MySQL JDBC Driver not found !!");
+			System.out.println("MySQL JDBC Driver not found !!"); //找不到JDBC Driver  
 			return;
 		}
-		System.out.println("MySQL JDBC Driver Registered!");
+		System.out.println("MySQL JDBC Driver Registered!"); // 成功註冊
 
 		try {
 			connection = DriverManager // jdbc:mysql://localhost:3306/data
 					.getConnection("jdbc:mysql://localhost:3306/exchange?useUnicode=true&characterEncoding=UTF-8",user,pwd);
+			//登入資料庫
 			System.out.println("SQL Connection to database established!");
 
 		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
+			System.out.println("Connection Failed! Check output console"); //與資料庫連接失敗
 			System.out.println(e.getMessage());
 			return;
 		}
@@ -46,17 +48,23 @@ public  class DataBaseAdmin {
 		}
 
 	}
+	//用以下Select Query之method 
+	//String query = "select  * from table_name";
 	public static ResultSet selectDB(String query){
 		
 		ResultSet result = null;
 		try{
 		PreparedStatement statement = connection.prepareStatement(query);
-		result = statement.executeQuery();
+		result = statement.executeQuery(); //將 SELECT結果存放至result 
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
-		return result;
+		return result; 
 	} 
+	//用以insert, update, delete 之method 
+	//UPDATE favorites SET type_name = 'swim' where account = 'admin'
+	//INSERT INTO Customers " + "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)
+	//DELETE FROM Registration " +"WHERE id = 101";
 	public  static void updateDB(String query){
 		
 		try{
@@ -71,10 +79,12 @@ public  class DataBaseAdmin {
 	
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-		ResultSet result = null;
-		DataBaseAdmin.openConnection("root","root");
-		String query = "UPDATE favorites SET type_name = 'swim' where account = 'admin'";
-		DataBaseAdmin.updateDB(query);
+		
+		// method 使用範例 僅供參考
+		ResultSet result = null; //用以承接Select結果
+		DataBaseAdmin.openConnection("root","root"); //建立連線, 輸入資料庫帳號密碼
+		String query = "UPDATE favorites SET type_name = 'swim' where account = 'admin'"; //QUERY
+		DataBaseAdmin.updateDB(query); 
 		
 	}
 
