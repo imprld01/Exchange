32ce92108aeaab2976ce3bdfe97bef24960c1160
Bo-Wun Shih <bowns1688@hotmail.com>
2016-12-30 22:44:00 +0800

    add file: Secret.java

---
+++ src/exchange/model/account/Secret.java
@@ -0,0 +1,13 @@
+package exchange.model.account;
+
+public class Secret {
+
+	private String id;
+	private String password;
+	
+	public Secret(String id, String password){
+		
+		this.id = id;
+		this.password = password;
+	}
+}
--- src/exchange/web/sign/LogInServlet.java
+++ src/exchange/web/sign/LogInServlet.java
@@ -1,11 +1,15 @@
 package exchange.web.sign;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import exchange.model.sign.SignManager;
 
 /**
  * Servlet implementation class LogInServlet
@@ -13,29 +17,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LogInServlet")
 public class LogInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogInServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
 	}
-
 }
