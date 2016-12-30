1d998c07ba504ae02c1710ea21900f2f6a264932
Bo-Wun Shih <bowns1688@hotmail.com>
2016-12-30 22:59:27 +0800

    modify: draft: LogInServlet.java

--- src/exchange/web/sign/LogInServlet.java
+++ src/exchange/web/sign/LogInServlet.java
@@ -12,9 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import exchange.model.account.Secret;
 import exchange.model.sign.SignManager;
 
-/**
- * Servlet implementation class LogInServlet
- */
 @WebServlet("/LogInServlet")
 public class LogInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
