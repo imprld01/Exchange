829432907efc5faf4265795571291882933d0f9d
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-02 19:31:42 +0800

    modify: ShowHomePageServlet.java: try to setAttribute

--- src/exchange/web/home/ShowHomePageServlet.java
+++ src/exchange/web/home/ShowHomePageServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import exchange.model.account.AccountManager;
+import exchange.model.account.Profile;
+import exchange.model.skill.SkillManager;
 
 @WebServlet("/Home.do")
 public class ShowHomePageServlet extends HttpServlet {
@@ -25,16 +27,16 @@ public class ShowHomePageServlet extends HttpServlet {
 			
 			String uid = (String)session.getAttribute("uid");
 			
-			/*
+			
 			AccountManager am = new AccountManager();
-			Profile profile = am.getAccount(uid);
+			Profile profile = am.getAccount(uid).getProfile();
 			
 			SkillManager sm = new SkillManager();
-			ArrayList<Skill> skills = sm.getAllSkills(uid);
+			//ArrayList<Skill> skills = sm.getAllSkills(uid);
 			
 			request.setAttribute("profile", profile);
-			request.setAttribute("skills", skills);
-			*/
+			//request.setAttribute("skills", skills);
+			
 			
 			view = request.getRequestDispatcher("/HomePage.jsp");
 			view.forward(request, response);
