78347b524915dfebac7cb765a643eef5cf8b07f7
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-02 19:21:15 +0800

    draft: AccountServlet.java without testing

--- src/exchange/web/account/AccountServlet.java
+++ src/exchange/web/account/AccountServlet.java
@@ -10,42 +10,48 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import exchange.model.account.AccountManager;
+import exchange.model.account.Profile;
+import exchange.model.account.Secret;
 
 @WebServlet("/Account.do")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	private static final int SECRET_MODIFICATION = 0;
+	private static final int PROFILE_MODIFICATION = 1;
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//only for modification
 		HttpSession session = request.getSession();
 		
 		if(session.isNew()){
-			String id = (String)request.getParameter("id");
-			String pwd = (String)request.getParameter("pwd");
-			String user = (String)request.getParameter("user");
-			String nick = (String)request.getParameter("nick");
-			String email = (String)request.getParameter("email");
-			String birth = (String)request.getParameter("birth");
-			String region = (String)request.getParameter("region");
-			
-			//Profile profile = new Profile(user, nick, email, birth, region);
-			
-			AccountManager sm = new AccountManager();
-			/*boolean checkResult = sm.checkPassword(secret);
-			
-			if(checkResult){
-				//session
-				session.setAttribute("uid", secret.getId());
-				session.setMaxInactiveInterval(1800);
-			}
-			else{
-				session.invalidate();
-				response.sendRedirect("index.html");
-			}
-			*/
-		}
 		
-		response.sendRedirect("/Home.do");
+			AccountManager am = new AccountManager();
+			int mark = Integer.parseInt((String)request.getParameter("mark"));
+			
+			switch(mark){
+			case SECRET_MODIFICATION:
+				String id = (String)request.getParameter("id");
+				String pwd = (String)request.getParameter("pwd");
+				
+				Secret secret = new Secret(id, pwd);
+				
+				am.setSecret(secret);
+				
+				response.sendRedirect("/Home.do");
+				break;
+			case PROFILE_MODIFICATION:
+				String nick = (String)request.getParameter("nick");
+				String email = (String)request.getParameter("email");
+				String region = (String)request.getParameter("region");
+				
+				Profile profile = new Profile(nick, email, region);
+				
+				am.setProfile(profile);
+				
+				response.sendRedirect("/Home.do");
+				break;
+			}
+		}
 	}
 }
