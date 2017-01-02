a29216ac810c6f0a6e5f276b5bb1f4bb5cb698c3
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-02 16:17:58 +0800

    draft: ShowHomePageServlet.java

--- src/exchange/web/home/ShowHomePageServlet.java
+++ src/exchange/web/home/ShowHomePageServlet.java
@@ -1,11 +1,16 @@
 package exchange.web.home;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import exchange.model.account.AccountManager;
 
 @WebServlet("/Home.do")
 public class ShowHomePageServlet extends HttpServlet {
@@ -13,6 +18,27 @@ public class ShowHomePageServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		HttpSession session = request.getSession(false);
 		
+		if(session != null){
+			RequestDispatcher view = null;
+			
+			String uid = (String)session.getAttribute("uid");
+			
+			/*
+			AccountManager am = new AccountManager();
+			Profile profile = am.getAccount(uid);
+			
+			SkillManager sm = new SkillManager();
+			ArrayList<Skill> skills = sm.getAllSkills(uid);
+			
+			request.setAttribute("profile", profile);
+			request.setAttribute("skills", skills);
+			*/
+			
+			view = request.getRequestDispatcher("/HomePage.jsp");
+			view.forward(request, response);
+		}
+		else response.sendRedirect("index.html");
 	}
 }
