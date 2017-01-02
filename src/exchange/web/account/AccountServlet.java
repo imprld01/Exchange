82f5aded88c4873d15c08f3c8e2b9cae20daf5cf
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-02 16:45:35 +0800

    draft modification: AccountServlet.java only have the ability to modify an account

--- src/exchange/web/account/AccountServlet.java
+++ src/exchange/web/account/AccountServlet.java
@@ -11,17 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	private static final int CREATION = 0;
-	private static final int MODIFICATION = 1;
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int mark = Integer.parseInt((String)request.getParameter("mark"));
-		
-		switch(mark){
-		case CREATION: break;
-		case MODIFICATION: break;
-		default: ;
-		}
+		//only for modification
 	}
 }
