1f4cc9e86339e4670921fce32edb9f5f4de3967a
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-02 16:43:38 +0800

    draft: SkillServlet.java(not complete)

--- src/exchange/web/skill/SkillServlet.java
+++ src/exchange/web/skill/SkillServlet.java
@@ -11,10 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 public class SkillServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	
+	private static final int CREATION = 0;
+	private static final int MODIFICATION = 1;
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		int mark = Integer.parseInt((String)request.getParameter("mark"));
 		
+		switch(mark){
+		case CREATION: break;
+		case MODIFICATION: break;
+		default: ;
+		}
 	}
 }
