e93c8ee292c644beeb7dfc2a8f40acf0c11730e8
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-03 11:13:23 +0800

    protected to public, composing, notation cleaning, and webservlet annotation correction

--- src/exchange/web/skill/SkillServlet.java
+++ src/exchange/web/skill/SkillServlet.java
@@ -9,12 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Skill.do")
 public class SkillServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 	
 	private static final int CREATION = 0;
 	private static final int MODIFICATION = 1;
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		int mark = Integer.parseInt((String)request.getParameter("mark"));
 		
