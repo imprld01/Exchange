ab84e7721ab114505057573699bb0a34522378d6
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-05 13:50:38 +0800

    draft: SkillServlet.java with some difficulty: (1)Type Constructor (2)Skill Constructor (3)Miss SkillManager Methods

--- src/exchange/web/skill/SkillServlet.java
+++ src/exchange/web/skill/SkillServlet.java
@@ -1,6 +1,7 @@
 package exchange.web.skill;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import exchange.model.skill.Skill;
 import exchange.model.skill.SkillManager;
+import exchange.model.skill.Type;
 
 @WebServlet("/Skill.do")
 public class SkillServlet extends HttpServlet {
@@ -24,24 +27,42 @@ public class SkillServlet extends HttpServlet {
 		HttpSession session = request.getSession(false);
 		
 		if(session != null){
-		
-			String ie, type, img, vdo;
+			
+			Type type;
+			int vn, in;
+			Skill skill;
+			String cid, ie;
 			SkillManager sm = new SkillManager();
+			ArrayList<String> img = new ArrayList<String>();
+			ArrayList<String> vdo = new ArrayList<String>();
 			int mark = Integer.parseInt((String)request.getParameter("mark"));
 			
 			switch(mark){
 			case CREATION:				
-				ie = (String)session.getAttribute("introExper");
-				type = (String)request.getParameter("type");
-				img = (String)session.getAttribute("image");
-				vdo = (String)request.getParameter("video");
+				ie = (String)request.getParameter("introExper");
+				//type = new Type((String)request.getParameter("type"));
+				vn = Integer.parseInt((String)request.getParameter("vnum"));
+				in = Integer.parseInt((String)request.getParameter("inum"));
+				for(int i = 1; i <= in; ++i) img.add((String)request.getParameter("image" + i));
+				for(int i = 1; i <= vn; ++i) vdo.add((String)request.getParameter("video" + i));
+				
+				//skill = new Skill(ie, type, img, vdo);
+				
+				//sm.createSkill(skill);
 				
 				response.sendRedirect("/Home.do");
 				break;
 			case MODIFICATION:
-				ie = (String)session.getAttribute("introExper");
-				img = (String)session.getAttribute("image");
-				vdo = (String)request.getParameter("video");
+				cid = (String)request.getParameter("cid");
+				ie = (String)request.getParameter("introExper");
+				vn = Integer.parseInt((String)request.getParameter("vnum"));
+				in = Integer.parseInt((String)request.getParameter("inum"));
+				for(int i = 1; i <= in; ++i) img.add((String)request.getParameter("image" + i));
+				for(int i = 1; i <= vn; ++i) vdo.add((String)request.getParameter("video" + i));
+				
+				//skill = new Skill(cid, ie, img, vdo);
+				
+				//sm.modifySkill(skill);
 				
 				response.sendRedirect("/Home.do");
 				break;
