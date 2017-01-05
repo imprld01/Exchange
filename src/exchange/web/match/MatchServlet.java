09fe553e523c373bf842774f26da9ad5932a853b
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-05 15:33:39 +0800

    draft: MatchServlet.java with some difficulty: (1)match() return void error

--- src/exchange/web/match/MatchServlet.java
+++ src/exchange/web/match/MatchServlet.java
@@ -1,11 +1,17 @@
 package exchange.web.match;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import exchange.model.match.BasicAlgorithm;
+import exchange.model.skill.Skill;
 
 @WebServlet("/Match.do")
 public class MatchServlet extends HttpServlet {
@@ -14,6 +20,24 @@ public class MatchServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		RequestDispatcher view = null;
+		HttpSession session = request.getSession(false);
 		
+		if(session != null){
+			
+			String uid = (String)session.getAttribute("uid");
+			String cid = (String)request.getParameter("cardId");
+			
+			BasicAlgorithm ba = new BasicAlgorithm(uid);
+			//Skill skill = ba.match();
+			
+			request.setAttribute("para", "cardId");
+			request.setAttribute("cid", cid);
+			//request.setAttribute("skill", skill);
+			
+			view = request.getRequestDispatcher("/MatchPage.jsp");
+			view.forward(request, response);
+		}
+		else response.sendRedirect("index.html");
 	}
 }
