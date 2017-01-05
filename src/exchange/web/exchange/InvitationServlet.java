77ff6edfdea180f16146b5c2bcb03f752e0fb3fc
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-05 14:33:03 +0800

    fix: miss the info of the exchanged card id to finish

--- src/exchange/web/exchange/InvitationServlet.java
+++ src/exchange/web/exchange/InvitationServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import exchange.model.exchange.ExchangeManager;
 
 @WebServlet("/Invitation.do")
 public class InvitationServlet extends HttpServlet {
@@ -14,5 +17,31 @@ public class InvitationServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		HttpSession session = request.getSession(false);
+		
+		if(session != null){
+			
+			String cid = (String)request.getParameter("cardId");
+			int attitude = Integer.parseInt((String)request.getParameter("atd"));
+			int profession = Integer.parseInt((String)request.getParameter("pfn"));
+			int teaching = Integer.parseInt((String)request.getParameter("tch"));
+			int frequency = Integer.parseInt((String)request.getParameter("fqc"));
+			int satisfication = Integer.parseInt((String)request.getParameter("sfn"));
+			String comment = (String)request.getParameter("comment");
+			
+			//EvaluationManager em = new EvaluationManager(cid);
+			//em.saveAttitudeScore(attitude);
+			//em.saveProfessionScore(profession);
+			//em.saveTeachingScore(teaching);
+			//em.saveFrequencyScore(frequency);
+			//em.saveSatisficationScore(satisfication);
+			//em.saveComment(comment);
+			
+			ExchangeManager xm = new ExchangeManager();
+			//xm.finishExchange(cid);
+			
+			response.sendRedirect("/Exchange.do");
+		}
+		else response.sendRedirect("index.html");
 	}
 }
