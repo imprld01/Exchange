f8a969291bbe38acf9d7935f5b898734d70f0782
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-06 19:07:57 +0800

    final draft: SkillServlet.java with a little refactoring

--- src/exchange/web/skill/SkillServlet.java
+++ src/exchange/web/skill/SkillServlet.java
@@ -3,7 +3,6 @@ package exchange.web.skill;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Hashtable;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -33,11 +32,10 @@ public class SkillServlet extends HttpServlet {
 		HttpSession session = request.getSession(false);
 		
 		if(session != null){
-			
-			Type type = null;
 			int vn, in;
 			Skill skill;
-			String cid, ie, id, uid, tp;
+			Type type = null;
+			String cid, ie, uid, tp;
 			ArrayList<String> img = new ArrayList<String>();
 			ArrayList<String> vdo = new ArrayList<String>();
 			int mark = Integer.parseInt((String)request.getParameter("mark"));
@@ -60,8 +58,6 @@ public class SkillServlet extends HttpServlet {
 				skill = new Skill(uid, ie, type, img, vdo);
 				
 				SkillManager.createSkill(skill);
-				
-				response.sendRedirect("/Home.do");
 				break;
 			case MODIFY_SKILL:
 				try {
@@ -80,36 +76,45 @@ public class SkillServlet extends HttpServlet {
 				skill = new Skill(cid, ie, type, img, vdo);
 				
 				SkillManager.modifySkill(skill);
-				
-				response.sendRedirect("/Home.do");
-				break;
-			case SHOW_SKILL:
-				RequestDispatcher view = null;
-				//Hashtable<String, Skill> table = (Hashtable<String, Skill>)session.getAttribute("skills");
-				
-				id = (String)request.getParameter("id");
-				Skill skilltoshow = new Skill(Integer.parseInt(id));
-				
-				request.setAttribute("skill", skilltoshow);
-				
-				view = request.getRequestDispatcher("/SkillPage.jsp");
-				view.forward(request, response);
 				break;
 			case CREATE_FAVORITE:
 				tp = (String)request.getParameter("type");
 				uid = (String)session.getAttribute("uid");
 				
 				SkillManager.createFavoriteSkill(uid, tp);
-				
-				response.sendRedirect("/Home.do");
 				break;
 			case DELETE_FAVORITE:
 				tp = (String)request.getParameter("type");
 				uid = (String)session.getAttribute("uid");
 				
 				SkillManager.deleteFavoriteSkill(uid, tp);
+				break;
+			}
+			
+			response.sendRedirect("/Home.do");
+		}
+		else response.sendRedirect("index.html");
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(false);
+		
+		if(session != null){
+			int mark = Integer.parseInt((String)request.getParameter("mark"));
+			
+			switch(mark){
+			case SHOW_SKILL:
+				RequestDispatcher view = null;
+				//Hashtable<String, Skill> table = (Hashtable<String, Skill>)session.getAttribute("skills");
 				
-				response.sendRedirect("/Home.do");
+				String id = (String)request.getParameter("id");
+				Skill skilltoshow = new Skill(Integer.parseInt(id));
+				
+				request.setAttribute("skill", skilltoshow);
+				
+				view = request.getRequestDispatcher("/SkillPage.jsp");
+				view.forward(request, response);
 				break;
 			}
 		}
