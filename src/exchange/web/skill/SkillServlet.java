0151926b07d6c6d702c6b2dbeca7e49cd5d8bf5b
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-06 18:59:10 +0800

    confirm the interface to SkillManager

--- src/exchange/web/skill/SkillServlet.java
+++ src/exchange/web/skill/SkillServlet.java
@@ -1,6 +1,7 @@
 package exchange.web.skill;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -33,32 +34,42 @@ public class SkillServlet extends HttpServlet {
 		
 		if(session != null){
 			
-			Type type;
+			Type type = null;
 			int vn, in;
 			Skill skill;
-			String cid, ie, id, uid;
-			SkillManager sm = new SkillManager();
+			String cid, ie, id, uid, tp;
 			ArrayList<String> img = new ArrayList<String>();
 			ArrayList<String> vdo = new ArrayList<String>();
 			int mark = Integer.parseInt((String)request.getParameter("mark"));
 			
 			switch(mark){
 			case CREATE_SKILL:
+				try {
+					type = new Type((String)request.getParameter("type"));
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				uid = (String)session.getAttribute("uid");
 				ie = (String)request.getParameter("introExper");
-				//type = new Type((String)request.getParameter("type"));
 				vn = Integer.parseInt((String)request.getParameter("vnum"));
 				in = Integer.parseInt((String)request.getParameter("inum"));
 				for(int i = 1; i <= in; ++i) img.add((String)request.getParameter("image" + i));
 				for(int i = 1; i <= vn; ++i) vdo.add((String)request.getParameter("video" + i));
 				
-				//skill = new Skill(ie, type, img, vdo);
+				skill = new Skill(uid, ie, type, img, vdo);
 				
-				//sm.createSkill(uid, skill);
+				SkillManager.createSkill(skill);
 				
 				response.sendRedirect("/Home.do");
 				break;
 			case MODIFY_SKILL:
+				try {
+					type = new Type((String)request.getParameter("type"));
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				cid = (String)request.getParameter("cid");
 				ie = (String)request.getParameter("introExper");
 				vn = Integer.parseInt((String)request.getParameter("vnum"));
@@ -66,36 +77,37 @@ public class SkillServlet extends HttpServlet {
 				for(int i = 1; i <= in; ++i) img.add((String)request.getParameter("image" + i));
 				for(int i = 1; i <= vn; ++i) vdo.add((String)request.getParameter("video" + i));
 				
-				//skill = new Skill(cid, ie, img, vdo);
+				skill = new Skill(cid, ie, type, img, vdo);
 				
-				//sm.modifySkill(skill);
+				SkillManager.modifySkill(skill);
 				
 				response.sendRedirect("/Home.do");
 				break;
 			case SHOW_SKILL:
 				RequestDispatcher view = null;
-				Hashtable<String, Skill> table = (Hashtable<String, Skill>)session.getAttribute("skills");
+				//Hashtable<String, Skill> table = (Hashtable<String, Skill>)session.getAttribute("skills");
 				
 				id = (String)request.getParameter("id");
+				Skill skilltoshow = new Skill(Integer.parseInt(id));
 				
-				request.setAttribute("skill", table.get(id));
+				request.setAttribute("skill", skilltoshow);
 				
 				view = request.getRequestDispatcher("/SkillPage.jsp");
 				view.forward(request, response);
 				break;
 			case CREATE_FAVORITE:
-				//type = new Type((String)request.getParameter("type"));
+				tp = (String)request.getParameter("type");
 				uid = (String)session.getAttribute("uid");
 				
-				//sm.createFavorite(uid, skill);
+				SkillManager.createFavoriteSkill(uid, tp);
 				
 				response.sendRedirect("/Home.do");
 				break;
 			case DELETE_FAVORITE:
-				//type = new Type((String)request.getParameter("type"));
+				tp = (String)request.getParameter("type");
 				uid = (String)session.getAttribute("uid");
 				
-				//sm.deleteFavrite(uid, skill);
+				SkillManager.deleteFavoriteSkill(uid, tp);
 				
 				response.sendRedirect("/Home.do");
 				break;
