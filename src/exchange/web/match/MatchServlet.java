f49748e6918af147ffbe2776378730efa377554e
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-06 21:59:33 +0800

    fix: doPost -> doGet

--- src/exchange/web/match/MatchServlet.java
+++ src/exchange/web/match/MatchServlet.java
@@ -18,7 +18,7 @@ public class MatchServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		RequestDispatcher view = null;
 		HttpSession session = request.getSession(false);
