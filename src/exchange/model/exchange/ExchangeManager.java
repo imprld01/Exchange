c202d80eec2fff6cbff5e21335b3b8053a103895
LuoYouRen <s9016010@gmail.com>
2017-01-06 17:30:26 +0800

    ExchangeManager finish

--- src/exchange/model/exchange/ExchangeManager.java
+++ src/exchange/model/exchange/ExchangeManager.java
@@ -37,13 +37,13 @@ public class ExchangeManager {
 		ResultSet exchangeResultB = DataBaseAdmin.selectDB("SELECT skill_a,skill_b,type_name FROM exchanges  join skills on exchanges.skill_b = skills.skill_id where end_flag = '0' AND"
 				+"(skill_b in (select skill_id from skills where user_id = '"+userID+"'))");
 		try{
-			//處理我送出的邀請 並放進ArrayList
+			//處理我送出的邀請所進行的交流 並放進ArrayList
 			while(exchangeResultA.next()){
 				Exchange exchange = new Exchange(new Skill(exchangeResultA.getInt("skill_b")), exchangeResultA.getString("type_name"));
 				//System.out.println("A"+exchangeResultA.getInt("skill_b")+exchangeResultA.getString("type_name"));
 				exchangeList.add(exchange);
 			}
-			//處理我收到的邀請 並放進ArrayList
+			//處理我收到的邀請所進行的交流 並放進ArrayList
 			while(exchangeResultB.next()){
 				Exchange exchange = new Exchange(new Skill(exchangeResultB.getInt("skill_a")), exchangeResultB.getString("type_name"));
 				//System.out.println("B"+exchangeResultB.getInt("skill_a")+exchangeResultB.getString("type_name"));
@@ -55,16 +55,36 @@ public class ExchangeManager {
 		DataBaseAdmin.closeConnection();
 		return exchangeList;
 	}
+	//列出所有收到的邀請並回傳ArrayList
 	public static ArrayList<Exchange> getReceiveInvitations(String userID){
 		ArrayList<Exchange> exchangeList = new ArrayList<Exchange>(); //回傳用
 		ResultSet receiveInvitationResult = DataBaseAdmin.selectDB("SELECT ivt_receiver,ivt_sender,type_name FROM invitations "
 				+"join skills on invitations.ivt_receiver = skills.skill_id "
 				+"where ivt_receiver in (select skill_id from skills where user_id = '"+userID+"')");
 		try{
-			//處理我送出的邀請 並放進ArrayList
+			//處理我收到的邀請 並放進ArrayList
 			while(receiveInvitationResult.next()){
 				Exchange exchange = new Exchange(new Skill(receiveInvitationResult.getInt("ivt_sender")), receiveInvitationResult.getString("type_name"));
-				System.out.println(receiveInvitationResult.getInt("ivt_sender")+receiveInvitationResult.getString("type_name"));
+				//System.out.println(receiveInvitationResult.getInt("ivt_sender")+receiveInvitationResult.getString("type_name"));
+				exchangeList.add(exchange);
+			}
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+		}
+		DataBaseAdmin.closeConnection();
+		return exchangeList;
+	}
+	//列出所有送出的邀請並回傳ArrayList
+	public static ArrayList<Exchange> getSendInvitations(String userID){
+		ArrayList<Exchange> exchangeList = new ArrayList<Exchange>(); //回傳用
+		ResultSet sendInvitationResult = DataBaseAdmin.selectDB("SELECT ivt_receiver,ivt_sender,type_name FROM invitations "
+				+"join skills on invitations.ivt_sender = skills.skill_id "
+				+"where ivt_sender in (select skill_id from skills where user_id = '"+userID+"')");
+		try{
+			//處理我送出的邀請 並放進ArrayList
+			while(sendInvitationResult.next()){
+				Exchange exchange = new Exchange(new Skill(sendInvitationResult.getInt("ivt_receiver")), sendInvitationResult.getString("type_name"));
+				System.out.println(sendInvitationResult.getInt("ivt_receiver")+sendInvitationResult.getString("type_name"));
 				exchangeList.add(exchange);
 			}
 		}catch(Exception e){
@@ -74,6 +94,6 @@ public class ExchangeManager {
 		return exchangeList;
 	}
 	public static void main(String args[]){
-		ExchangeManager.getReceiveInvitations("vegetable");
+		ExchangeManager.getSendInvitations("bowen");
 	}
 }
