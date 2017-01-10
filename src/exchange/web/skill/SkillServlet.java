a9da9d42c821fc2839186add533a5de27bf6bf9c
vegetableting <alex100433@gmail.com>
2017-01-11 04:48:36 +0800

    .

--- src/exchange/web/skill/SkillServlet.java
+++ src/exchange/web/skill/SkillServlet.java
@@ -68,6 +68,7 @@ public class SkillServlet extends HttpServlet {
 				SkillManager.modifySkill(skill);
 				break;
 			case CREATE_FAVORITE:
+				System.out.println("CREATE_FAVORITE");
 				tp = (String)request.getParameter("type");
 				uid = (String)session.getAttribute("uid");
 				
