bf153754cfeb192f09efc80a308e8b7bdb76152a
speed4560123 <a0970870239@gmail.com>
2017-01-14 16:14:35 +0800

    .

--- src/exchange/model/evaluation/EvaluationManager.java
+++ src/exchange/model/evaluation/EvaluationManager.java
@@ -11,24 +11,37 @@ import exchange.model.skill.SkillManager;
 
 public class EvaluationManager{
 	
-	public static void saveComment(int skillId, String comment) throws SQLException
+	public static boolean saveComment(int skillId, String comment) throws SQLException
 	{
-		
+		boolean result = false;
 		Date recentLog = new Date();
 		java.sql.Date now = new java.sql.Date(recentLog.getTime());
-		String query = "INSERT comments SET comment = '"+ comment +"',date = '"+ now +"' where skill_id = '"+skillId+"'";
-		DataBaseAdmin.updateDB(query);
+		try{
+			String query = "INSERT comments SET comment = '"+ comment +"',date = '"+ now +"' where skill_id = '"+skillId+"'";
+		    DataBaseAdmin.updateDB(query);
+		    result = true;
+		}catch(Exception e){
+			
+		}
+		return result;
 	}
 
-	public static void saveScore(int skillId, Score score) throws SQLException
+	public static boolean saveScore(int skillId, Score score) throws SQLException
 	{
+		boolean result = false;
 		Skill skill = SkillManager.findSkill(skillId);
-		String query = "UPDATE skills SET attitude_score = '"+ (skill.getScore().getFrequency() + score.getAttitude()) +"',"
-				+ " profession_score = '"+ (skill.getScore().getProfession() + score.getProfession()) +"', "
-				+ "teaching_score = '"+(skill.getScore().getTeaching() + score.getTeaching())+"',"
-				+ " frequency_score = '"+(skill.getScore().getFrequency() + score.getFrequency()) +"', "
-				+ "satisfication_score = '"+ (skill.getScore().getSatisfication() + score.getSatisfication()) +"'"
-				+ "where skill_id = '"+skillId+"'";
-		DataBaseAdmin.updateDB(query);
+		try{
+			String query = "UPDATE skills SET attitude_score = '"+ (skill.getScore().getFrequency() + score.getAttitude()) +"',"
+			+ " profession_score = '"+ (skill.getScore().getProfession() + score.getProfession()) +"', "
+			+ "teaching_score = '"+(skill.getScore().getTeaching() + score.getTeaching())+"',"
+			+ " frequency_score = '"+(skill.getScore().getFrequency() + score.getFrequency()) +"', "
+			+ "satisfication_score = '"+ (skill.getScore().getSatisfication() + score.getSatisfication()) +"'"
+			+ "where skill_id = '"+skillId+"'";
+		    DataBaseAdmin.updateDB(query);
+		    result = true;
+		}catch(Exception e){
+			
+		}
+		return result;
 	}
 }
