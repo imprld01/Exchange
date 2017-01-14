89fdf6840a0e7cba403d1f859eb896936acfe480
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-14 14:26:31 +0800

    Area.java indent

--- src/exchange/model/match/Area.java
+++ src/exchange/model/match/Area.java
@@ -1,16 +1,22 @@
 package exchange.model.match;
 
 class Area{ //依照表格排序地區  (魔術數字)
+	
 	private int distance;
 	private String placeName;
-	Area(int distance, String placeName) { this.distance=distance; this.placeName=placeName; }
 	
-	public int getDistance(){
+	Area(int distance, String placeName) {
+		
+		this.distance=distance; this.placeName=placeName;
+	}
+	
+	public int getDistance() {
+		
 		return distance;
 	}
 
-	public String getPlaceName(){
+	public String getPlaceName() {
+		
 		return placeName;
 	}
-
 }
